7d944b27f5853f7ebe218764b6deef86a65d7388
supunlakshan100 <lakshans793@gmail.com>
2024-05-14 16:21:13 +0530

    folder structure added

--- README.md
+++ README.md
@@ -19,7 +19,6 @@ pip install -r requirements.txt
 
 ```ini
 PINECONE_API_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-PINECONE_API_ENV = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 ```
 
 
@@ -29,3 +28,8 @@ PINECONE_API_ENV = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 ## Download the Llama 2 Model:
 
 llama-2-7b-chat.ggmlv3.q4_0.bin
+
+## From the following link:
+https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML/tree/main
+```
+
---
+++ template.py
@@ -0,0 +1,39 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+list_of_files = [
+    "src/__init__.py",
+    "src/helper.py",
+    "src/prompt.py",
+    ".env",
+    "research/trails.ipynb",
+    "app.py",
+    "store_index.py", 
+    "static/.gitkeep",
+    "templates/chat.html",   
+     
+
+    ]
+
+# create files and directories
+for filepath in list_of_files:
+    filepath=Path(filepath)
+    filedir,filename=os.path.split(filepath)
+
+    # create directory if it does not exist
+    if filedir!='':
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file {filename}")
+
+    # create file if it does not exist
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0): # create file if it does not exist
+        with open(filepath, 'w') as f:
+            logging.info(f"Creating empty file; {filepath}")
+    
+    # log if file already exists
+    else:
+        logging.info(f"{filename} is already created")
+    
